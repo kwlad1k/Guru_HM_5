775b35899a4ef41922da4518356d04c21eedf1f2
Kwlad1ck <vlad-kad00@mail.ru>
2024-01-14 17:23:22 +0300

    delete second option

--- src/test/java/guru.qa/CodeInsideThePageTest.java
+++ src/test/java/guru.qa/CodeInsideThePageTest.java
@@ -29,17 +29,4 @@ public class CodeInsideThePageTest {
 //      $$(".markdown-body h4").shouldHave(itemWithText("3. Using JUnit5 extend test class:"));
     }
 
-    @Test
-    void secondTest() {
-        open("https://github.com"); // Открытие гита
-        $(".header-search-button > .flex-1").click(); // Открытие поискового окна
-        $("#query-builder-test").setValue("Selenide").pressEnter(); // Поиск селенида
-        $x("//em[contains(.,'selenide')]").click();// Выбор селенида
-        $("#wiki-tab").click();// Выбор вики
-        $(".js-wiki-more-pages-link").click();
-        $("#wiki-pages-box").shouldHave(text("SoftAssertions")).click();//Нажатие SoftAssertions
-//        $("#user-content-without-soft-assertions-you-will-get-report").scrollIntoView(false); // Скролл до кода JUnit5
-//        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class")); // Проверка наличия кода JUnit5
-    }
-
 }
